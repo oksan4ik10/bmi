fcda32a5ce8ae53294b50686be74aa6947ed968d
kalinina Oksana <ksan4ik92@gmail.com>
2021-11-09 11:08:08 +0300

    add page home and write result form

--- flask-test.py
+++ flask-test.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, escape
+from main import bmi
 
 app = Flask(__name__)
 
@@ -8,9 +9,23 @@ def hi()->'html':
     return render_template('index.html', the_title='ИМТ', display = 'd-none')
 
 
-@app.route('/', methods=['POST'])
+@app.route('/home', methods=['POST'])
 def test():
-    return render_template('index.html', the_title='ИМТ hjfgskhl', display = '')
+  
+    user = request.form['username']
+    age = int(request.form['age'])
+    height = float(request.form['height'])
+    weight = float(request.form['weight'])
+
+    if request.form.get('save'):
+        save = True
+    else:
+        save = False
+    
+    result = bmi(height, weight, user, age, save)
+
+    return render_template('index.html', the_title='ИМТ результат', display = '', result = result)
+    
 
 
 if __name__=='__main__':
--- main.py
+++ main.py
@@ -1,42 +1,42 @@
+def bmi(user_height, user_weight, name, age, save = False) -> str:
+    results = {}
 
-#основная программа
-user_height = 1.60
-user_weight = 77.8
-name = 'Oksana'
-age = 28
+    bmi = round(user_weight/(user_height**2),2)
 
-results = {}
-
-bmi = round(user_weight/(user_height**2),2)
-
-print('Ваш ИМТ составляет: ', bmi,2)
-if bmi <=16:
-    res = ('Выраженный дефицит массы тела')
-elif bmi <=18.5:
-    res = ('Недостаточная (дефицит) масса тела')
-elif bmi <=25:
-    res = ('Норма')
-elif bmi <=30:
-    res = ('Избыточная масса тела (предожирение)')
-elif bmi <= 35:
-    res = ('Ожирение 1 степени')
-elif bmi <= 40:
-    res = ('Ожирение 2 степени')
-else:
-    res = ('Ожирение 3 степени')
+    print('Ваш ИМТ составляет: ', bmi)
+    if bmi == 0.0:
+        return 'Данные введены не корректно'
+    if bmi <=16:
+        res = ('Выраженный дефицит массы тела')
+    elif bmi <=18.5:
+        res = ('Недостаточная (дефицит) масса тела')
+    elif bmi <=25:
+        res = ('Норма')
+    elif bmi <=30:
+        res = ('Избыточная масса тела (предожирение)')
+    elif bmi <= 35:
+        res = ('Ожирение 1 степени')
+    elif bmi <= 40:
+        res = ('Ожирение 2 степени')
+    else:
+        res = ('Ожирение 3 степени')
 
 
-#запись и чтение из файла
-results = [name,age, user_height, user_weight, bmi, res]
-print (results)
+    #запись и чтение из файла
+    results = [name,age, user_height, user_weight, bmi, res]
+    print (results)
 
+    if save:
+        with open('./file.txt','a') as file:
+            for i in results:
+                item = str(i)
+                file.write(item)
+                file.write(' ')
+            file.write('\n')
+    
+    return res
 
-with open('file.txt','a') as file:
-    for i in results:
-        item = str(i)
-        file.write(item)
-        file.write(' ')
-    file.write('\n')
+# print(bmi(1.64, 78, 'Oksana', 27))
 
 with open('file.txt', 'r') as file:
     content = file.readlines()
