2ebc76c684978f0f2601a5f8cf34ecc4d1e8361b
Оксана Калинина <kalininaO@SPK.LOCAL>
2021-11-09 09:31:56 +0300

    add d-none for label

--- flask-test.py
+++ flask-test.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hi()->'html':
-    return render_template('index.html', the_title='ИМТ')
+    return render_template('index.html', the_title='ИМТ', display = 'd-none')
 
 
 @app.route('/', methods=['POST'])
 def test():
-    return render_template('index.html', the_title='ИМТ hjfgskhl')
+    return render_template('index.html', the_title='ИМТ hjfgskhl', display = '')
 
 
 if __name__=='__main__':
