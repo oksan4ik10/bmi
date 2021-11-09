c7550bc8f80380a9cf4c7c91989484bb89344ca0
Oksana Kalinina <ksan4ik92@gmail.com>
2021-11-09 21:51:18 +0300

    add danger for not correction

--- flask-test.py
+++ flask-test.py
@@ -6,16 +6,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def hi()->'html':
-    return render_template('index.html', the_title='ИМТ', display = 'd-none')
+    return render_template('index.html', the_title='ИМТ', display = 'd-none', done = 'd-none')
 
 
 @app.route('/home', methods=['POST'])
 def test():
-  
+    print(request.form)
     user = request.form['username']
+    age = request.form['age']
+    height = request.form['height']
+    weight = request.form['weight']
+
+    if (not user) or (not age) or (not height) or (not weight):
+        return render_template('index.html', the_title='ИМТ результат', done='',display = 'd-none', result = 'Не все данные были заполнены')
+
     age = int(request.form['age'])
     height = float(request.form['height'])
     weight = float(request.form['weight'])
+
 
     if request.form.get('save'):
         save = True
@@ -24,9 +32,9 @@ def test():
     
     result = bmi(height, weight, user, age, save)
 
-    return render_template('index.html', the_title='ИМТ результат', display = '', result = result)
+    return render_template('index.html', the_title='ИМТ результат', display = '',done = 'd-none', result = result)
     
-@app.route('/result')
+@app.route('/results')
 def print_result():
     tables = print_table()
     return render_template('results.html', the_title='Результаты', tables = tables, count = len(tables))
